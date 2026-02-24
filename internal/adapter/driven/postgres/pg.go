c11973cd057eb897eb7685fd683eee863e060aa3
Alexandr <sashkaageev@gmail.com>
2026-02-24 17:28:10 +0300

    added postgres storage

--- internal/adapter/driven/postgres/pg.go
+++ internal/adapter/driven/postgres/pg.go
@@ -1,1 +1,50 @@
 package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	DSN             string
+	MaxConns        int32
+	MinConns        int32
+	MaxConnLifetime time.Duration
+	MaxConnIdleTime time.Duration
+}
+
+type Storage struct {
+	pool *pgxpool.Pool
+	log  *zap.Logger
+}
+
+func New(ctx context.Context, cfg *Config, log *zap.Logger) (*Storage, error) {
+	pgConfig, err := pgxpool.ParseConfig(cfg.DSN)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse dsn: %w", err)
+	}
+	pgConfig.MaxConns = cfg.MaxConns
+	pgConfig.MinConns = cfg.MinConns
+	pgConfig.MaxConnLifetime = cfg.MaxConnLifetime
+	pgConfig.MaxConnIdleTime = cfg.MaxConnIdleTime
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create db pool: %w", err)
+	}
+	if err = pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+	return &Storage{
+		pool: pool,
+		log:  log,
+	}, nil
+}
+
+func (s *Storage) Close() {
+	s.pool.Close()
+}
