1c806398c587a98af1034b8cc76045d658e9eed9
Alexandr <sashkaageev@gmail.com>
2026-03-02 20:23:40 +0300

    created skeleton of auth service

--- internal/app/auth/auth.go
+++ internal/app/auth/auth.go
@@ -1,1 +1,33 @@
 package auth
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/sanchey92/sso/internal/domain/model"
+)
+
+type Hasher interface {
+	Hash(password string) (string, error)
+	Verify(password, encodedHash string) (bool, error)
+}
+
+type UserRepository interface {
+	Create(ctx context.Context, user *model.User) error
+	GetByEmail(ctx context.Context, email string) (*model.User, error)
+}
+
+type Service struct {
+	hasher   Hasher
+	userRepo UserRepository
+	log      *zap.Logger
+}
+
+func New(h Hasher, ur UserRepository, log *zap.Logger) *Service {
+	return &Service{
+		hasher:   h,
+		userRepo: ur,
+		log:      log,
+	}
+}
